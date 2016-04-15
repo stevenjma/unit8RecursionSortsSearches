2873e11c40864e028d4d534c508f5c80afe6ad6c
stevenjma <SJMa@student.naperville203.org>
2016-04-15 10:30:04 -0500

    Almost done with sorter

--- RadixSort/RadixSorter.java
+++ RadixSort/RadixSorter.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class RadixSorter
 {
     public static void sort(int[] a)
@@ -11,9 +12,40 @@ public class RadixSorter
             }
         }
         int iterations = greatest.toString().length();
-        for (int i = 0; i < iterations; i++)
+        int [][] z = new int[iterations][a.length];
+        ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>(10);
+        for (int i = z.length - 1; i >= 0; i--)
         {
-            
+            for (int k = 0; k < 10; k++)
+            {
+                buckets.add(new ArrayList<Integer>());
+            }
+            for (int k = 0; k < 10; k++)
+            {      
+                for (int j = 0; j < z[i].length; j++)
+                {
+                    Integer y = a[j];
+                    String numAsStr = String.valueOf(y);
+                    for (int l = 0; l < (iterations - numAsStr.length()); l++)
+                    {
+                        numAsStr = "0" + numAsStr;
+                    }
+                    z[i][j] = Integer.parseInt(numAsStr.substring(i, i + 1));
+                    if (z[i][j] == k)
+                    {
+                        buckets.get(k).add((z[i][j]));
+                    }
+                }
+            }
+        }
+        
+        ArrayList<Integer> filler = new ArrayList<>();
+        
+        for (int i = 0; i < buckets.size(); i++)
+        {
+            for (int j = 0; j < buckets.get(i).size(); j++)
+            {
+            }
         }
     }
 }
